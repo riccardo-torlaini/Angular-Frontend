eb809aded227bdc6cee6dc2e792528f5b67ac8ca
Mihai-Dragoş <dragos200019@yahoo.com>
2021-10-29 14:00:11 +0200

    Added Company Opportunity for DNB (#79)
    - added DNB to the list
    - added Traineeship as an option to the category of company opportunity now
    - added DNB picture into the right folder
    
    Co-authored-by: Mihai-Dragoş <m.ungureanu1@student.tue.nl>

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,6 +3,7 @@ export class AppConstants {
     // Company opportunity related constants
     public static companyOpportunityCompanies = new Map([
         ["Optiver", "./assets/img/partners/optiver.png"],
+        ["De Nederlandsche Bank (DNB)", "./assets/img/partners/dnb.png"],
     ]);
-    public static companyOpportunityCategories = ["internship", "vacancy"];
+    public static companyOpportunityCategories = ["internship", "vacancy", "traineeship"];
 }
